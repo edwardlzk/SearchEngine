6ca272d322651bde5d349534c65cd86c11444baf
sissixw <sissi_xw@live.cn>
2013-04-02 14:19:34 -0400

    m

---
+++ src/edu/nyu/cs/cs2580/Prf.java
@@ -0,0 +1,24 @@
+package edu.nyu.cs.cs2580;
+
+import java.util.Vector;
+
+import edu.nyu.cs.cs2580.ScoredDocument;
+
+public class Prf {
+	int numterms;
+	Vector<ScoredDocument> scoredDocs;
+	public Prf(Vector<ScoredDocument> scoredDocs,int numterms) {
+		// TODO Auto-generated constructor stub
+		this.scoredDocs = scoredDocs;
+		this.numterms = numterms;		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
