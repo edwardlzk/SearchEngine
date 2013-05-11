8ada345447a4171756e0b3eddb30e731b8ac15e8
Liu Mengyao <banduo@LiumatoMacBook-Air.local>
2013-05-10 21:50:09 -0400

    m

--- src/edu/nyu/cs/cs2580/IndexerMapper.java
+++ src/edu/nyu/cs/cs2580/IndexerMapper.java
@@ -1,17 +1,19 @@
 package edu.nyu.cs.cs2580;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SortedMapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class IndexerMapper extends Mapper<LongWritable, Text, Text, IntWritable>
+public class IndexerMapper extends Mapper<LongWritable, Text, Text, SortedMapWritable>
 {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
+        SortedMapWritable m=new SortedMapWritable();
         
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
@@ -21,7 +23,8 @@ public class IndexerMapper extends Mapper<LongWritable, Text, Text, IntWritable>
                 while(tokenizer.hasMoreTokens())
                 {
                         word.set(tokenizer.nextToken());
-                        context.write(word, one);
+                        
+                        context.write(word,m);
                 }
         }
 }
--- src/edu/nyu/cs/cs2580/OccurrenceReducer.java
+++ src/edu/nyu/cs/cs2580/OccurrenceReducer.java
@@ -1,6 +1,9 @@
 package edu.nyu.cs.cs2580;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -8,12 +11,13 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class OccurrenceReducer extends Reducer<Text, IntWritable, Text, IntWritable>
 {
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+        public void reduce(Text key, Iterable<Map.Entry<K, V>> values, Context context) throws IOException, InterruptedException
         {
                 int sum = 0;
-                for(IntWritable value: values)
+                Iterator<Map> it=values.iterator();
+                while(it.hasNext())
                 {
-                        sum += value.get();
+                        sum = it.next().size();
                 }
                 context.write(key, new IntWritable(sum));
         }
